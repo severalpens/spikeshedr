5333af5867a6600400b1b1bed7633bae2f625ca2
pacollin1 <paul.collins1@ausnetservices.com.au>
2024-08-08 12:58:13 +1000

    chore: Add react-router-dom dependency and update routing files

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,23 @@
-import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import SwimTimeCreateForm from '../ui-components/SwimTimeCreateForm'
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import SwimTimes from "./components/SwimTimes";
 
 const client = generateClient<Schema>();
 
 
 function App() {
-  const [swimTimes, setSwimTimes] = useState<Array<Schema["SwimTime"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.SwimTime.observeQuery().subscribe({
-      next: (data) => setSwimTimes([...data.items]),
-    });
-  }, []);
-
-  // function createSwimTime() {
-  //   client.models.SwimTime.create({ content: window.prompt("SwimTime content") });
-  // }
-  
-  function deleteSwimTime(id: string) {
-    client.models.SwimTime.delete({ id })
-  }
-
   return (
-        
-    <Authenticator>
-      {({ signOut, user }) => (
-    <main>
-          <h1>{user?.signInDetails?.loginId}'s swimTimes</h1>
-          <SwimTimeCreateForm />
-      <ul>
-        {swimTimes.map((swimTime) => (
-          <li onClick={() => deleteSwimTime(swimTime.id)}
-           key={swimTime.id}>{swimTime.SwimDate}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new swimTime.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
-        
-      )}
-      </Authenticator>
+    <div className="container mx-auto font-sans">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="swimtimes" element={<SwimTimes/>} />
+      </Routes>
+    </div>
   );
 }
 
