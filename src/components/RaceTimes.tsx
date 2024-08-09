8cf69540a383f945e32b6e9be5a4247b2b33a653
pacollin1 <paul.collins1@ausnetservices.com.au>
2024-08-09 13:14:10 +1000

    chore: Update RaceTimes component to use table for displaying race times

--- src/components/RaceTimes.tsx
+++ src/components/RaceTimes.tsx
@@ -27,16 +27,32 @@ function RaceTimes() {
         <main>
           <h1>{user?.signInDetails?.loginId}'s raceTimes</h1>
           <RaceTimeCreateForm />
-          <ul>
-            {raceTimes.sort((a, b) => {
-              const dateA = a.RaceDate ? new Date(a.RaceDate.toString()) : null;
-              const dateB = b.RaceDate ? new Date(b.RaceDate.toString()) : null;
-              return dateA && dateB ? dateA.getTime() - dateB.getTime() : 0;
-            }).map((raceTime) => (
-              <li key={raceTime.id}>{raceTime.RaceDistance} | {raceTime.RaceDate} | {raceTime.RaceMins}:{raceTime.RaceSecs}</li>
-            ))}
-          </ul>
-          <button onClick={signOut}>Sign out</button>
+          <table className="table-auto">
+            <thead>
+              <tr>
+                <th className="border px-4 py-2">Race Distance</th>
+                <th className="border px-4 py-2">Race Date</th>
+                <th className="border px-4 py-2">Race Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {raceTimes.sort((a, b) => {
+                const dateA = a.RaceDate ? new Date(a.RaceDate.toString()) : null;
+                const dateB = b.RaceDate ? new Date(b.RaceDate.toString()) : null;
+                return dateA && dateB ? dateA.getTime() - dateB.getTime() : 0;
+              }).map((raceTime) => (
+                <tr key={raceTime.id}>
+                  <td className="border px-4 py-2">{raceTime.RaceDistance}</td>
+                  <td className="border px-4 py-2">{raceTime.RaceDate}</td>
+                  <td className="border px-4 py-2">{raceTime.RaceMins}:
+                  {raceTime.RaceSecs != null ? (raceTime.RaceSecs < 10 ? `0${raceTime.RaceSecs}` : raceTime.RaceSecs) : 0}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button onClick={signOut} className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Sign out
+          </button>
         </main>
       )}
     </Authenticator>
