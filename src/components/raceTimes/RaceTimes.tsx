7eadf29bbfa18822c18f496e672de0aadfba18ef
pacollin1 <paul.collins1@ausnetservices.com.au>
2024-08-09 18:00:13 +1000

    refactor: Update RaceTimes component to hide prepopulate button

--- src/components/raceTimes/RaceTimes.tsx
+++ src/components/raceTimes/RaceTimes.tsx
@@ -84,7 +84,7 @@ function RaceTimes() {
               setShowPrepopulate(false);
           }}
               className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              hidden={showPrepopulate}
+              hidden={!showPrepopulate}
             >
               Prepopulate using old race times
             </button>
