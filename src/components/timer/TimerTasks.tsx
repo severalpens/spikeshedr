5371015d8a12e34eb002eaa383d16734bbbf530c
pacollin1 <paul.collins1@ausnetservices.com.au>
2024-08-15 17:46:34 +1000

    refactor: Update TimerProjects component to improve UI and functionality

--- src/components/timer/TimerTasks.tsx
+++ src/components/timer/TimerTasks.tsx
@@ -1,7 +1,23 @@
+import { useEffect,  useState } from "react";
+import type { Schema } from "../../../amplify/data/resource";
+// import regression from 'highcharts-regression';
+import { generateClient } from "aws-amplify/data";
 import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import TimerTaskCreateForm from '../../../ui-components/TimerTaskCreateForm';
 import { Link } from "react-router-dom";
+const client = generateClient<Schema>();
 
 function TimerTasks() {
+  const [timerProjects, setTimerProjects] = useState<Array<Schema["TimerProject"]["type"]>>([]);
+  const [showForm, setShowForm] = useState<boolean>(false);
+ 
+  useEffect(() => {
+    client.models.TimerProject.observeQuery().subscribe({
+      next: (data) => setTimerProjects([...data.items]),
+    });
+  }, []);
+
 
   return (
     <Authenticator>
@@ -11,8 +27,44 @@ function TimerTasks() {
           <div id="breadcrumb" className="mb-8">
             <Link to="/timer">Timer</Link> &gt; Tasks
           </div>
-          <div>
-            TBD
+          <div id="newTimeForm" className="mb-12">
+            <button
+              onClick={() => setShowForm(!showForm)}
+              className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  w-48"
+            >
+              {showForm ? 'Hide New Item Form' : 'Add New'}
+            </button>
+            {showForm && <TimerTaskCreateForm />}
+          </div>
+          <div id="timerTasksTable" >
+            <div className="flex justify-end mb-4">
+            </div>
+            <div className="overflow-x-auto">
+              <table className="table-auto w-full" >
+                <thead>
+                  <tr>
+                    <th className="border px-4 py-2">Project Name</th>
+                    <th className="border px-4 py-2">Task Name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {timerProjects
+                    .map((timerProject) => (
+                      Array.isArray(timerProject.TimerTasks) && timerProject.TimerTasks.map((timerTask) => (
+                        <tr key={timerTask.id}>
+                          <td className="border px-4 py-2">
+                            <Link to={`/timer/projects/${timerProject.id}`}>{timerProject.Name}</Link>
+                          </td>
+                          <td className="border px-4 py-2">
+                            <Link to={`/timer/projects/${timerProject.id}/tasks/${timerTask.id}`}>{timerTask.name}</Link>
+                          </td>
+                        </tr>
+                      ))
+                    ))}
+
+                </tbody>
+              </table>
+            </div>
           </div>
           <div className="flex justify-end">
           <button onClick={signOut} className="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  w-48">
@@ -20,7 +72,6 @@ function TimerTasks() {
           </button>
             
           </div>
-
         </main>
       )}
     </Authenticator>
