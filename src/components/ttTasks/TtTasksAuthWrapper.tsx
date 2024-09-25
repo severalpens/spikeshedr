7fba4a4c721c5dae32606421f16b2c91f8382c81
pacollin1 <paul.collins1@ausnetservices.com.au>
2024-09-25 11:18:59 +1000

    Refactor TtTasksAuthWrapper component

--- src/components/ttTasks/TtTasksAuthWrapper.tsx
+++ src/components/ttTasks/TtTasksAuthWrapper.tsx
@@ -10,7 +10,7 @@ function TtTasksAuthWrapper() {
       {({ signOut, user }) => {
         return (
           <div className="mt-5">
-            {user && <TtTasks user={user} />}
+            {user && <TtTasks  />}
 
             <div className="flex justify-end">
               <button onClick={signOut} className="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  w-48">
